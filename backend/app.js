0fe47b0b8ab836f1e1c077778c57ad96ea3d7b44
legolover7 <67564539+legolover7@users.noreply.github.com>
2023-11-16 15:56:00 -0500

    PLEASE

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ mongoose.connection.once('open', () => {
 
 // Routes
 app.use('/api', require('./routes'));
-
+app.response.setHeader("Access-Control-Allow-Origin", '*')
 
 // Start server
 app.listen(process.env.DB_PORT, () => {
